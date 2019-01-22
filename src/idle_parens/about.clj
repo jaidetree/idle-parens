cd18fe9d55c4b631bacaf4c5ecbaa5597aa55f9f
Jay <jayzawrotny@gmail.com>
2019-01-22 13:09:27 -0500

    Made interests collapsible, WIP on experience

--- src/idle_parens/about.clj
+++ src/idle_parens/about.clj
@@ -1,6 +1,57 @@
 (ns idle-parens.about
   (:require [hiccup.page :refer [html5]]
-            [idle-parens.base :as base]))
+            [idle-parens.base :as base]
+            [idle-parens.formats :refer [slugify]]))
+
+(defn interest
+  [& {:keys [title media content columns]}]
+  (let [slug (slugify title)]
+    [:li.interest
+     [:header.interest__header
+      [:h3.interest__title title]
+      [:button.toggle
+       {:type "button"
+        :data-toggle "collapse"
+        :data-target (str "#" slug)
+        :aria-expanded "false"
+        :aria-controls slug}
+       [:span.d-none "Toggle Interest"]]]
+     [:div.interest__content.collapse
+      {:class (when columns "row") :id slug}
+      (when media
+        [:div.interest__media
+         {:class (when columns "col-4")}
+         media])
+      [:p.text-copy.interest__copy
+       {:class (when columns "col mt-4")}
+       content]]]))
+
+(defn skill
+  [& {:keys [title value years content keywords]}]
+  (let [slug (slugify title)]
+   [:li.skill
+    [:header.skill__header
+     [:h4.skill__title title]
+     [:button.toggle
+      {:type "button"
+       :data-toggle "collapse"
+       :data-target (str "#" slug)
+       :aria-expanded "false"
+       :aria-controls slug}
+      [:span.d-none "Toggle Interest"]]
+     [:div.skill__progress
+      [:button.skill__value
+       {:style (str "width:" value "%;")
+        :type "button"}
+       [:span.skill__label
+        (str value "%")]]]]
+    [:div.skill__content.collapse
+     {:id slug}
+     [:ul.skill__keywords
+      (for [kw keywords]
+        [:li.skill__keyword kw])]
+     [:p.text-copy.skill__copy
+      content]]]))
 
 (defn render
   [{global-meta :meta posts :entries}]
@@ -11,72 +62,147 @@
    :content [:div.about
              [:h1 "About Eccentric J"]
              [:p.text-copy
-              "I've been designing and developing personal and commercial projects for about a decade now. I got started by learning how to hack online computer games by rewriting values to known memory addresses, since then I've found my passion in web app development. Currently, I'm employed as a lead engineer at "
+              "I've been designing and developing personal and commercial projects for about a decade now. I got started by learning how to hack online computer games by rewriting values to known memory addresses, since then I've found my passion in web app development. Currently, I live in New York City employed as a lead engineer at "
               [:a {:href "https://venuebook.com"}
                "VenueBook"]
               " where I am responsible for software architecture, development, and product design."]
-             [:p.callout
-              "At VenueBook we are looking to hire a senior Clojure developer and a senior Python\\Devops engineer. If you are interested or know someone who might be: Please reach out to me."]
+             [:div.callout
+              [:h3.borders.padding "Looking for Engineers"]
+              [:p "VenueBook is hiring both a senior Clojure developer and a senior Python\\Devops engineer. Please reach out if you are interested or know someone who might be."]]
              [:h2 "Interests"]
-             [:ul.interests
-              [:li.interest
-               [:h3.interest__title "Atmospheric Black Metal"]
-               [:div.interest__media
-                [:iframe {:width 560
-                          :height 315
-                          :src "https://www.youtube.com/embed/videoseries?list=UUDLkzWN1rHY4eYkGnVruHVw"
-                          :frameborder 0
-                          :allow "autoplay; encrypted-media"
-                          :allowfullscreen true}]]
-               [:p.text-copy.interest__copy
-                "Ah metal. Such a rich genre with so much creative potential. Of all the subgenres I find myself drawn most to the alluring spell of atomspheric black metal. A vast subgenre with a rich technicality to the instrumentation, complex layers of melody and spirit, and evocative themes. The biting edge of black metal vocals layered on top is like some kind of blood-soaked cherry on a deliciously ominous cupcake of abject pleasure."]]
+             [:ul.interests.borders
+              (interest
+               :title "Atmospheric Black Metal"
+               :media [:iframe {:width 560
+                                :height 315
+                                :src "https://www.youtube.com/embed/videoseries?list=UUDLkzWN1rHY4eYkGnVruHVw"
+                                :frameborder 0
+                                :allow "autoplay; encrypted-media"
+                                :allowfullscreen true}]
+               :content "Ah metal. Such a rich genre with so much creative potential. Of all the subgenres I find myself drawn most to the alluring spell of atomspheric black metal. A vast subgenre with a rich technicality to the instrumentation, complex layers of melody and spirit, and evocative themes. The biting edge of black metal vocals layered on top is like some kind of blood-soaked cherry on a deliciously ominous cupcake of abject pleasure.")
 
-              [:li.interest
-               [:h3.interest__title "Reading"]
-               [:div.row
-                [:div.interest__media.col-4
-                 [:a.display-block
-                  {:href "https://www.braveclojure.com/clojure-for-the-brave-and-true/"}
-                  [:img.interest__book.d-block
-                   {:src "/img/cbt.png"
-                    :alt "Clojure for the Brave and True"}]]]
-                [:p.text-copy.interest__copy.col.mt-4
-                 "Books are really important to me, they&rsquo;re how I got into programming. I try to read every day when I can: Articles, books, comics, and even text-books. At the time of writing I've been on a sci-fi kick, please send any recommendations you may have."]]]
+              (interest
+               :title "Reading"
+               :columns true
+               :media
+                [:a.display-block
+                 {:href "https://www.braveclojure.com/clojure-for-the-brave-and-true/"}
+                 [:img.interest__book.d-block
+                  {:src "/img/cbt.png"
+                   :alt "Clojure for the Brave and True"}]]
+               :content "Books are really important to me, they&rsquo;re how I got into programming. I try to read every day when I can: Articles, books, comics, and even text-books. At the time of writing I've been on a sci-fi kick, please send any recommendations you may have.")
 
-              [:li.interest
-               [:h3.interest__title "Video Games"]
-               [:div.interest__media
-                [:iframe {:width 560
-                          :height 315
-                          :src "https://www.youtube.com/embed/uYZ8mrnb_wg"
-                          :frameborder 0
-                          :allow "autoplay; encrypted-media"
-                          :allowfullscreen true}]]
-               [:p.text-copy.interest__copy
-                "Video games are the best. I love exploring imaginary worlds while practicing creative problem solving within inviting narratives. It's a beautiful thing when done well but for Hyper Light Drifter featured above? Doubly so."]]
+              (interest
+               :title "Video Games"
+               :media [:iframe {:width 560
+                                :height 315
+                                :src "https://www.youtube.com/embed/uYZ8mrnb_wg"
+                                :frameborder 0
+                                :allow "autoplay; encrypted-media"
+                                :allowfullscreen true}]
+               :content "Video games are the best. I love exploring imaginary worlds while practicing creative problem solving within inviting narratives. It's a beautiful thing when done well but for Hyper Light Drifter featured above? Doubly so.")
 
-              [:li.interest
-               [:h3.interest__title "Bullet Journaling"]
-               [:div.interest__media
-                [:img
-                 {:src "/img/eccentric-j-bujo.jpg"
-                  :alt "Eccentric J's 2019 Bullet Journal: January Month Spread"}]]
-               [:p.text-copy.interest__copy
-                "Introduced to the idea from a friend, I&rsquo;ve been bullet-journaling for going on 3 years now. It helps me build good habits and track my daily tasks while also encouraging balance between work, goals, and personal life."]]
+              (interest
+               :title "Bullet Journaling"
+               :media [:a
+                       {:href "https://bulletjournal.com/"
+                        :target "_blank"}
+                       [:img
+                        {:src "/img/eccentric-j-bujo.jpg"
+                         :alt "Eccentric J's 2019 Bullet Journal: January Month Spread"}]]
+               :content "Introduced to the idea from a friend, I&rsquo;ve been bullet-journaling for going on 3 years now. It helps me build good habits and track my daily tasks while also encouraging balance between work, goals, and personal life.")
 
-              [:li.interest
-               [:h3.interest__title "Art"]
-               [:div.interest__media
-                [:iframe#instagram-media.instagram-media-rendered
-                 {:allowfullscreen "true",
-                  :style
-                  "background: white none repeat scroll 0% 0%; max-width: 720px; width: calc(100% - 2px); border-radius: 3px; border: 1px solid rgb(219, 219, 219); box-shadow: none; display: block; margin: 0px 0px 12px; min-width: 326px; padding: 0px;"
-                  :src
-                  "https://www.instagram.com/p/BGvfID7rHwm/embed/?cr=1&v=12&wp=720"
-                  :frameborder "0"
-                  :allowtransparency "true"
-                  :data-instgrm-payload-id "instagram-media-payload-0"
-                  :scrolling "no"
-                  :height "700"}]]
-               [:p.text-copy.interest__copy
-                "An interest I accquired later in life. I enjoy discovering artists through social media, going to art gallery openings in NYC, drawing youtube channels, and the occasional doodle. Admittedly, I'm not very good at drawing but I do enjoy learning it in waves every once in a while."]]]]))
+              (interest
+               :title "Art"
+               :media [:iframe.instagram-media-rendered
+                       {:allowfullscreen "true",
+                        :style
+                        "background: white none repeat scroll 0% 0%; max-width: 720px; width: calc(100% - 2px); border-radius: 3px; border: 1px solid rgb(219, 219, 219); box-shadow: none; display: block; margin: 0px 0px 12px; min-width: 326px; padding: 0px;"
+                        :src
+                        "https://www.instagram.com/p/BGvfID7rHwm/embed/?cr=1&v=12&wp=720"
+                        :frameborder "0"
+                        :allowtransparency "true"
+                        :data-instgrm-payload-id "instagram-media-payload-0"
+                        :scrolling "no"
+                        :height "700"}]
+               :content "An interest I accquired later in life. I enjoy discovering artists through social media, going to art gallery openings in NYC, drawing youtube channels, and the occasional doodle. Admittedly, I'm not very good at drawing but I do enjoy learning it in waves every once in a while.")]
+             [:div.section
+              [:h2 "Experience"]
+              [:ul.skills.borders
+               (skill
+                :title "Clojure"
+                :value 70
+                :years 1
+                :keywords ["lein" "boot" "luminus"]
+                :content "")
+
+               (skill
+                :title "ClojureScript"
+                :value 40
+                :years 0.5
+                :keywords ["figwheel-main" "lein-figwheel" "shadow-cljs"]
+                :content "")
+
+               (skill
+                :title "JavaScript"
+                :value 90
+                :years 10
+                :keywords ["functional"
+                           "FRP"
+                           "React"
+                           "Redux"
+                           "RxJS"
+                           "Ramda"
+                           "Vue"
+                           "Angular"
+                           "Node"
+                           "babel"]
+                :content "")
+
+               (skill
+                :title "HTML/CSS"
+                :value 90
+                :years 10
+                :keywords ["html5"
+                           "responsive"
+                           "grids"
+                           "bootstrap"
+                           "SMACSS"
+                           "atomic"
+                           "BEM"]
+                :content "")
+
+               (skill
+                :title "Design"
+                :value 80
+                :years 8
+                :keywords ["logos"
+                           "websites"
+                           "UI\\UX"
+                           "Collateral"]
+                :content "")
+
+               (skill
+                :title "Python"
+                :value 60
+                :years 5
+                :keywords ["django" "python 2.7" "flask"]
+                :content "")
+
+               (skill
+                :title "Ruby"
+                :value 30
+                :years 1
+                :keywords ["rails 3" "ruby 2"]
+                :content "")
+
+               (skill
+                :title "PHP"
+                :value 50
+                :years 9
+                :keywords ["php5"]
+                :content "")]]
+
+             [:div.section
+              [:h2 "Education"]
+              [:p.text-copy "I have a Bachelors in Digital Media from Kendall College of Art &amp; Design located in Grand Rapids, Michigan. I am a self-taught programmer and chose to focus on design in college to improve my weakest skill."]]]))
